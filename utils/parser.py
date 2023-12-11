47a85691bc40cde4d95a1a921553b7d920ca2771
Damian Kluczyński <damiankluczynski00@gmail.com>
2023-12-11 02:28:30 +0100

    added cmd parser

---
+++ utils/parser.py
@@ -0,0 +1,34 @@
+import optparse
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option(
+        "-m",
+        dest='model_name',
+        type='string',
+        default="model",
+        help="name of model",
+    )
+    optParser.add_option(
+        "--train",
+        action='store_true',
+        default=False,
+        help="training or testing",
+    )
+    optParser.add_option(
+        "-e",
+        dest='epochs',
+        type='int',
+        default=50,
+        help="Number of epochs",
+    )
+    optParser.add_option(
+        "-s",
+        dest='steps',
+        type='int',
+        default=500,
+        help="Number of steps",
+    )
+    options, args = optParser.parse_args()
+    return options
